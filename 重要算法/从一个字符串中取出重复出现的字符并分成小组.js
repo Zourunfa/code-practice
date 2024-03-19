4dd5481e76bd702d56072619eacb0f7bffab4112
Zourunfa <473023989@qq.com>
2024-03-19 23:26:49 +0800

    fix: review

--- 重要算法/从一个字符串中取出重复出现的字符并分成小组.js
+++ 重要算法/从一个字符串中取出重复出现的字符并分成小组.js
@@ -3,3 +3,8 @@ let str = 'sssdddsagrgrrrrrsaaa'
 let res = []
 let reg = /([a-z])\1+/g
 console.log(str.match(reg))
+
+// 写一个reduce方法的应用实际例子
+const numbers = [1, 2, 3, 4, 5]
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+console.log(sum) // 输出 15
