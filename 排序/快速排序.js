18dfcc0dc723e931ce05ab24123ab880cf74248b
Zourunfa <473023989@qq.com>
2024-04-16 23:16:08 +0800

    feat: 复习

--- 排序/快速排序.js
+++ 排序/快速排序.js
@@ -1,26 +1,55 @@
+// let quickSort = function (arr, left, right) {
+//   let point = Math.floor((left + right) / 2);
+//   let i = left,
+//     j = right;
+
+//   while (left < right) {
+//     while (arr[i] < point) {
+//       i++;
+//     }
+//     while (arr[j] > point) {
+//       j--;
+//     }
+
+//     if (left < right) {
+//       [arr[i], arr[j]] = [arr[j], arr[i]];
+//       i++;
+//       j--;
+//     }
+//     if (i < right) quickSort(arr, i, right);
+//     if (left < j) quickSort(arr, left, j);
+//   }
+// };
+
+// let a = [1, 2, 8, 5, 7, 9];
+// quickSort(a, 0, a.length - 1);
+// console.log(a);
+
+//
+
 let quickSort = function (arr, left, right) {
-  let point = Math.floor((left + right) / 2);
+  let point = Math.floor((left + right) / 2)
   let i = left,
-    j = right;
+    j = right
 
   while (left < right) {
     while (arr[i] < point) {
-      i++;
+      i++
     }
     while (arr[j] > point) {
-      j--;
+      j--
     }
 
     if (left < right) {
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-      i++;
-      j--;
+      ;[arr[i], arr[j]] = [arr[j], arr[i]]
+      i++
+      j--
     }
-    if (i < right) quickSort(arr, i, right);
-    if (left < j) quickSort(arr, left, j);
+    if (i < right) quickSort(arr, i, right)
+    if (left < j) quickSort(arr, left, j)
   }
-};
+}
 
-let a = [1, 2, 8, 5, 7, 9];
-quickSort(a, 0, a.length - 1);
-console.log(a);
+let a = [1, 2, 8, 5, 7, 9]
+quickSort(a, 0, a.length - 1)
+console.log(a)
